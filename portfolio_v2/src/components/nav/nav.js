cc5528dfcc31b086ffef508588d548234f7a5e79
KHL562 <khl562sk@gmail.com>
2019-10-16 19:07:08 -0700

    navi-icons

--- portfolio_v2/src/components/nav/nav.js
+++ portfolio_v2/src/components/nav/nav.js
@@ -1,39 +1,48 @@
 import React from 'react';
-// import logo from './logo.svg';
+import author from './author.png';
 import './nav.css';
+import { TiDeviceLaptop } from 'react-icons/ti';
+import { TiDevicePhone } from 'react-icons/ti';
+import { TiSocialGithub } from 'react-icons/ti';
+import { TiSocialLinkedin } from 'react-icons/ti';
+import { IconContext } from 'react-icons';
 
-function Nav() {
-  return (
-    <div className="Nav">
-        <nav href="#navbar" className=""></nav>
-        <aside className="">
-            <div className="text-center">
-              <div className="author-img"></div>
-              <h1><a href="index.html">Kevin Lee</a></h1>
-              <span className="email">kevinleehyun@gmail.com</span>
-            </div>
-            <nav role="navigation" className="navbar">
-              <div id="navbar" className="collapse">
-                <ul>
-                  <li className="active"><a href="#home" data-nav-section="home">Introduction</a></li>
-                  <li><a href="#about" data-nav-section="about">About</a></li>
-                  <li><a href="#timeline" data-nav-section="timeline">Timeline</a></li>
-                </ul>
+
+class Nav extends React.Component {
+  render() {
+    return (
+      
+      <div className="Nav">
+        <IconContext.Provider value={{ className: 'react-icons' }}>
+          <nav href="#navbar" className=""></nav>
+          <aside className="">
+              <div className="text-center">
+              <div className="author-img"><img src={author} alt="author-img"/></div>
+                <h1><a href="index.html">Kevin Lee</a></h1>
+                <div className="email"><TiDeviceLaptop /> kevinleehyun@gmail.com</div>
+                <div className="phone-number"><TiDevicePhone /> 562 405-2820</div>
               </div>
-            </nav>
-            <nav id="main-menu">
-              <ul>
-              <li><a href="https://www.facebook.com/dhruv.barochia" target="_blank" rel="noopener noreferrer"><i className="icon-facebook2" /></a></li>
-                <li><a href="https://twitter.com/ddbarochiya" target="_blank" rel="noopener noreferrer"><i className="icon-twitter2" /></a></li>
-                <li><a href="https://www.instagram.com/ddbarochiya/" target="_blank" rel="noopener noreferrer"><i className="icon-instagram" /></a></li>
-                <li><a href="https://www.linkedin.com/in/dhruv-b-545b52a9/" target="_blank" rel="noopener noreferrer"><i className="icon-linkedin2" /></a></li>
-                <li><a href="https://github.com/Dhruv34788" target="_blank" rel="noopener noreferrer"><i className="icon-github"></i></a></li>
-                <li><a href="https://medium.com/@dhruv.barochia34788" target="_blank" rel="noopener noreferrer"><i className="icon-blogger2"></i></a></li>
-              </ul>
-            </nav>
-        </aside>
-    </div>
-  );
+              <nav role="navigation" className="navbar">
+                <div id="navbar" className="collapse text-center">
+                  <ul>
+                    <li className="active"><a href="#home" data-nav-section="home">INTRODUCTION</a></li>
+                    <li><a href="#about" data-nav-section="about">ABOUT</a></li>
+                    <li><a href="#timeline" data-nav-section="timeline">TIMELINE</a></li>
+                  </ul>
+                </div>
+              </nav>
+              <nav id="main-menu">
+                <ul>
+                <li><a href="https://www.facebook.com/dhruv.barochia" target="_blank" rel="noopener noreferrer"><i className="icon-facebook2" /></a></li>
+                  <li><a href="https://www.instagram.com/ddbarochiya/" target="_blank" rel="noopener noreferrer"><i className="icon-instagram" /></a></li>
+                  <li><a href="https://www.linkedin.com/in/dhruv-b-545b52a9/" target="_blank" rel="noopener noreferrer"><TiSocialLinkedin /></a></li>
+                  <li><a href="https://github.com/Dhruv34788" target="_blank" rel="noopener noreferrer"><TiSocialGithub/></a></li>
+                </ul>
+              </nav>
+          </aside>
+          </IconContext.Provider>
+      </div>
+    )};
 }
 
 export default Nav;
